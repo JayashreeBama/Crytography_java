f602b186a5bb5379767e2e9168255a6633b4e659
Jayashree  p <165661550+JayashreeBama@users.noreply.github.com>
2025-04-22 01:53:15 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,6 @@
 import java.util.*;
 public class Hello{
   public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
-    String s=sc.next();
-    System.out.println(s);
+    System.out.println("Hello world);
   }
 }
