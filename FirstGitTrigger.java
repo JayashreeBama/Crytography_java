b7212bc97e8859749964d38884afc6a230585e3b
Jayashree  p <165661550+JayashreeBama@users.noreply.github.com>
2025-04-22 15:24:05 +0530

    Update FirstGitTrigger.java

--- FirstGitTrigger.java
+++ FirstGitTrigger.java
@@ -2,5 +2,5 @@ import java.util.*;
 public class FirstGitTrigger
 {
 public static void main(String args[]){
-System.out.println("Build Success");
+System.out.println("Build Success\nGood Luck!!");
 }}
